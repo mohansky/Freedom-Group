640455cbae26e032281d8c0407b07746eafbe1ec
mohansky <38560439+mohansky@users.noreply.github.com>
2023-03-27 10:25:46 +0530

    Delete components directory

--- components/display/page-title.js
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({ children }) {
-  return (
-    <h2 className="text-5xl text-center font-extrabold my-12">
-      {children}
-    </h2>
-  );
-}
--- components/social-links.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function SocialLinks({ URL, icon, alt }) {
-  return (
-    <a href={URL}>
-      <Image src={icon} alt={alt} className="h-8" width={30} height={30}/>
-    </a>
-  );
-}
