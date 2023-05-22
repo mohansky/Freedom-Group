8346ff185f7286ff2626b823749a075dfd2f333f
mohansky <38560439+mohansky@users.noreply.github.com>
2023-05-22 20:57:12 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -81,8 +81,24 @@ export default function Home({ posts }) {
         </section>
 
         <section className="relative px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16 lg:py-32">
-          <SectionTitle>Get in touch</SectionTitle>
+          <SectionTitle>Contact</SectionTitle>
           <ContactInfo />
+        </section>
+        <section className="py-20 px-5 bg-fgprimarybg">
+          <div className="my-20 mx-auto w-full md:w-3/4 lg:w-2/4">
+            <Form />
+          </div>
+        </section>
+
+        <section>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3451.5216162210054!2d78.30223285060838!3d30.10788418177021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3909161af640e745%3A0x182918b007f819a!2sChandreshwar%20Mandir%20Marg%2C%20Chandreshwar%20Nagar%2C%20Rishikesh%2C%20Uttarakhand%20249201!5e0!3m2!1sen!2sin!4v1678775663457!5m2!1sen!2sin"
+            width="100%"
+            height="640"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
         </section>
       </main>
     </>
