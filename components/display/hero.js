46192ae735ce04d30ce775dff83c53b54d59d21d
mohansky <38560439+mohansky@users.noreply.github.com>
2023-06-30 12:23:38 +0530

    Add files via upload

--- components/display/hero.js
+++ components/display/hero.js
@@ -7,21 +7,22 @@ export default function Hero({ item }) {
     <>
       <div className="relative px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-48 overflow-hidden flex items-center min-h-screen">
         <div className="w-full h-full absolute top-0 left-0 z-30 bg-black opacity-20"></div>
-        <div className="w-full h-full absolute top-0 left-0 z-20 bg-left-top blur-xs"> 
+        <div className="w-full h-full absolute top-0 left-0 z-20 bg-left-top "> 
+        {/* blur-xs */}
           <Image
             src={item.img}
             alt={item.title}
             width={1920}
             height={1080}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover grayscale "
           />
         </div>
 
         <div className="lg:w-3/4 xl:w-3/4 relative z-50 h-100 lg:mt-16">
-            <h1 className="glow text-fgprimary drop-shadow-lg shadow-white text-5xl md:text-7xl xl:text-8xl font-black leading-tight">
+            <h1 className="glow text-white drop-shadow-lg shadow-white text-5xl md:text-7xl xl:text-8xl font-black leading-tight">
               {item.title}
             </h1>
-            <p className="simple text-fgprimary drop-shadow-2xl text-xl md:text-4xl leading-snug mt-4 ">
+            <p className="simple text-white drop-shadow-2xl text-xl md:text-4xl leading-snug mt-4 ">
               {item.subtitle}
             </p>
             <Link href={item.btnUrl}>
